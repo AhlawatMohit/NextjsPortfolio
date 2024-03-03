489b606bc47cf33ae1b1dfc71c553bddd3284e30
Mohit Ahlawat <mohitahlawat223@gmail.com>
2024-03-03 17:13:23 +0530

    About Page completed

--- src/app/page.js
+++ src/app/page.js
@@ -6,32 +6,32 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration: 1.1}}>
-    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-36">
+    <motion.div className="h-full" initial={{ y: "-200vh" }} animate={{ y: "0%" }} transition={{ duration: 1.1 }}>
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-36">
 
-      {/* IMAGE CONTAINER */}
-      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-        <Image className="rounded-br-[30%] rounded-bl-[25%] lg:w-[28rem] lg:h-[30rem] md:h-[18rem] md:w-[22rem] md:ml-[15rem] xl:ml-[1rem] ml-16" src="/profile.png" alt="Profile Image" width={260} height={200} />
+        {/* IMAGE CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+          <Image className="rounded-br-[30%] rounded-bl-[25%] lg:w-[28rem] lg:h-[30rem] md:h-[18rem] md:w-[22rem] md:ml-[15rem] xl:ml-[1rem] ml-16" src="/profile.png" alt="Profile Image" width={260} height={200} />
+        </div>
+
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-6 items-center justify-center">
+          <div className="flex flex-col gap-1">
+            <p className="text-sm lg:text-lg">Hi, I'm</p>
+            <h1 className="text-3xl md:text-6xl font-bold text-[#4763ff]">Mohit Ahlawat</h1>
+            <h3 className="text-slate-600 text-lg">Frontend developer | MERN | UI/UX </h3>
+            <p className="text-base md:text-lg">I am looking for a Software Engineering role at a company which develop products with huge impact on people life.</p>
+          </div>
+          {/* BUTTONS */}
+          <motion.div initial={{ x: "100vh" }} animate={{ x: "-10%" }} transition={{ type: 'spring', delay: 1, stiffness: 200 }} className="flex gap-4">
+            <Link href="/resume.pdf">
+              <button className="p-2.5 rounded-lg ring-2 ring-[#9BB0C1] bg-black text-white">Resume/CV</button>
+            </Link>
+            <button className="p-2.5 rounded-lg ring-2 ring-black bg-white text-black">View My Work</button>
+          </motion.div>
+        </div>
+
       </div>
-
-      {/* TEXT CONTAINER */}
-      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-6 items-center justify-center">
-      <div className="flex flex-col gap-1">
-<p className="text-sm lg:text-lg">Hi, I'm</p>
-<h1 className="text-3xl md:text-6xl font-bold text-[#4763ff]">Mohit Ahlawat</h1>
-<h3 className="text-slate-600 text-lg">Frontend developer | MERN | UI/UX </h3>
-<p className="text-base md:text-lg">I am looking for a Software Engineering role at a company which develop products with huge impact on people life.</p>
-</div>
-{/* BUTTONS */}
-<motion.div initial={{x:"100vh"}} animate={{x:"-10%"}} transition={{type:'spring', delay:1, stiffness:200}} className="flex gap-4">
-<Link href="/resume.pdf">
-  <button className="p-2.5 rounded-lg ring-2 ring-[#9BB0C1] bg-black text-white">Resume/CV</button>
-  </Link>
-  <button className="p-2.5 rounded-lg ring-2 ring-black bg-white text-black">View My Work</button>
-</motion.div>
-      </div>
-
-    </div>
     </motion.div>
   );
 }
